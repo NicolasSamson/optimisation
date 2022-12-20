c6e8c1d22b985a4c1262ab261e794ddee7984ea0
NicolasSamson <nicolas.samson.3@ulaval.ca>
2022-12-19 21:58:05 -0500

    version_final_github

--- test_folder/Tangram_G_Code_Solved/analysis.py
+++ test_folder/Tangram_G_Code_Solved/analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np
 #df =pd.read_csv(r"C:\Users\nicsa\OneDrive - Université Laval\École\A 2022\Investigation\optimisation_2\test_folder\Tangram_G_Code_Solved\result_time\result_time_500_photo.csv")
-df = pd.read_csv(r"C:\Users\Proprio\OneDrive - Université Laval\École\A 2022\Investigation\optimisation_2\test_folder\Tangram_G_Code_Solved\result_time\result_time_500_photo_2.csv")
+df = pd.read_csv(r"C:\Users\Proprio\OneDrive - Université Laval\École\A 2022\Investigation\optimisation_2\test_folder\Tangram_G_Code_Solved\result_time\result_time_500_photo_3.csv")
 
 # extrait les 500 première lignes
 df= df.loc[df.indice_simulation <500]
@@ -23,10 +23,12 @@ print(df__not_inpalce)
 print(df__best_time_deux_methodes.loc[df__best_time_deux_methodes.indice_simulation==0])
 
 print("ratio de solution trouvée sur pas trouvée inplace",len(indice_simul_in_place)/500)
-print("ratio de solution trouvée sur pas trouvée not inplace",len(indice_simul_not_in_place)/500)
+print("ratio de solution trouvée sur pas trouvée not inplace",len(indice_simul_not_in_place)/500,len(indice_simul_not_in_place))
 list_diff_temp = []
 list_temp_inplace = []
 list_temp_not_inpalce = []
+
+list_nbr_sol_val_in_place = []
 for i in range(500):
     
     if i not in indice_simul_not_in_place or i not in indice_simul_in_place:
@@ -42,9 +44,22 @@ for i in range(500):
     #print(df_not_inplace)
     
     list_temp_not_inpalce.append(df_not_inplace.total_time.values[0])
+
+    # Extract inplace value
+    df_i_pas_best = df.loc[df.indice_simulation == i]
+    nbr_sol_val = df_i_pas_best.loc[df_i_pas_best.type=="inplace"]
+    nbr_sol_val = nbr_sol_val.shape[0]
+    #print(nbr_sol_val)
+    list_nbr_sol_val_in_place.append(nbr_sol_val)
     #list_diff_temp.append(df_inplace.total_time-df_not_inplace.total_time)
 #list_temp_inplace = np.array(list_temp_inplace)
 #list_temp_not_inpalce = np.array(list_temp_not_inpalce)
+# 3. Calcul toutes les solutions in place différentes
+# df_not_inplace 
+array_nbr_sol_val_inplace = np.array(list_nbr_sol_val_in_place)
+df_nbr_detection = pd.DataFrame.from_dict({"nbr_inpalce":array_nbr_sol_val_inplace})
+
+print(df_nbr_detection.describe())
 df_result = pd.DataFrame.from_dict({"best_time_inplace":list_temp_inplace,"best_time_not_implace":list_temp_not_inpalce})
 
 df_result.to_csv(r"C:\Users\Proprio\OneDrive - Université Laval\École\A 2022\Investigation\optimisation_2\test_folder\Tangram_G_Code_Solved\result_time\result_time_500_best_sol_2_hypothesis.csv")
